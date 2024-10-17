e33440708d441fa632c062bd04a6fa9afc3fd133
unknown <maima.alive25@gmail.com>
2024-10-17 22:25:17 +0500

    ,y first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+// syntax function(...name:string){}
+function sum() {
+    var a = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        a[_i] = arguments[_i];
+    }
+    console.log(a);
+}
+sum("Saima");
---
+++ main.ts
@@ -0,0 +1,7 @@
+// syntax function(...name:string){}
+function sum(...a:number[]){
+console.log(a)
+}
+sum(4*8-3)
+sum(6/6*4)
+sum(4+5*9)
